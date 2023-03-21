222d33572bbf240a49a2c956448a5b0567332293
chaipat supharak <chaipat.sup@cpf.co.th>
2023-03-22 00:38:53 +0700

    home work 2 task

--- cypress/e2e/shareCypress.cy.js
+++ cypress/e2e/shareCypress.cy.js
@@ -17,6 +17,24 @@ describe('template spec', () => {
     login(userName,password)
   });
 
+  it('login fail', function() {
+    cy.visit('https://acetoys.uk/our-story');
+    cy.get('#LoginLink').click();
+    cy.get('#username').type("test-valid-user");
+    cy.get('#password').type(password);
+    cy.get('.btn').click();
+    cy.get('.alert').should('be.visible').contains('Invalid credentials')
+  });
+
+  it('checkout product', function() {
+    login(userName,password)
+    cy.get('.list-group > :nth-child(1)').click()
+    cy.get(':nth-child(1) > [style="position: relative"] > p > .btn').click()
+    cy.get(':nth-child(3) > .btn-success').click()
+    cy.get('.text-right > .btn').click()
+    cy.get('.display-3').should('be.visible').contains("Order complete!")
+  });
+
   function login(userName,password){
     cy.visit('https://acetoys.uk/our-story');
     cy.get('#LoginLink').click();
